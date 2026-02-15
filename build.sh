9bb1714badf04b5791ab11ba262bf6ddfc172b8f
unknown <Abdelali.elyo@gmail.com>
2026-02-15 21:13:51 +0000

    Restore Railway configuration, remove Vercel files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip install Flask pandas numpy gunicorn python-dotenv
-pip install git+https://github.com/deepentropy/tvscreener.git
-pip install git+https://github.com/rongardF/tvdatafeed.git
--- vercel-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install pandas numpy python-dotenv
-pip install git+https://github.com/deepentropy/tvscreener.git
-pip install git+https://github.com/rongardF/tvdatafeed.git
-echo "Dependencies installed successfully"
