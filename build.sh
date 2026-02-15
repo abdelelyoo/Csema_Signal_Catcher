2b4c2e11caba86c07c6d6af477b64782f4676c81
unknown <Abdelali.elyo@gmail.com>
2026-02-15 21:00:34 +0000

    Add build script for git packages

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install Flask pandas numpy gunicorn python-dotenv
+pip install git+https://github.com/deepentropy/tvscreener.git
+pip install git+https://github.com/rongardF/tvdatafeed.git
